93d1fdb4257f268f44ca6855451acb2287fe5b02
Robin Aggleton <robin.aggleton@cern.ch>
2016-03-10 09:42:25 +0000

    Now have 3 configs: GT, DB, DB+ESProducers.
    Also tidy up scripts a bit
    Also move reference outputs to python dir, and tidy up.

---
+++ JetAnalyser/python/run_DB.py
@@ -0,0 +1,100 @@
+"""
+This config is for running with JEC in a SQL file.
+Note that we purposely use the *wrong* GlobalTag to ensure it's actually using the SQL file!
+
+GT:
+74X_mcRun2_asymptotic_v5 :  Summer15_25nsV7
+76X_mcRun2_asymptotic_RunIIFall15DR76_v1 : Fall15_25nsV2
+"""
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: py L1NtupleRECO -s RAW2DIGI --era=Run2_2016 --conditions=auto:run2_mc --filein=/store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/AODSIM/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/1E2B5203-7AAA-E511-B6FD-0CC47A4D762A.root --secondfilein=/store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/08F09D12-11AA-E511-9613-0025905A60D0.root --no_exec --no_output -n 200
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('JET',eras.Run2_2016)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(2)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('/store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/AODSIM/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/1E2B5203-7AAA-E511-B6FD-0CC47A4D762A.root'),
+    # secondaryFileNames = cms.untracked.vstring('/store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/08F09D12-11AA-E511-9613-0025905A60D0.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('py nevts:200'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+# process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '74X_mcRun2_asymptotic_v5', '')
+# process.GlobalTag = GlobalTag(process.GlobalTag, '76X_mcRun2_asymptotic_RunIIFall15DR76_v1', '')
+
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+from CondCore.DBCommon.CondDBSetup_cfi import *
+process.jec = cms.ESSource("PoolDBESSource",
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0)
+        ),
+    timetype = cms.string('runnumber'),
+    toGet = cms.VPSet(
+        cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            # tag    = cms.string('JetCorrectorParametersCollection_Summer15_25nsV7_MC_AK4PFchs'),
+            tag    = cms.string('JetCorrectorParametersCollection_Fall15_25nsV2_MC_AK4PFchs'),
+            label  = cms.untracked.string('AK4PFCHS')
+            ),
+        ),
+    # connect = cms.string('sqlite:Summer15_25nsV7_MC.db')
+    connect = cms.string('sqlite:Fall15_25nsV2_MC.db')
+)
+
+## add an es_prefer statement to resolve a possible conflict from simultaneous connection to a global tag
+process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+
+process.jetAnalysis = cms.EDAnalyzer('JetAnalyser')
+
+process.TFileService = cms.Service(
+    "TFileService",
+    # fileName = cms.string('L1Ntuple_summer15V7_200_wrongGT_db.root')
+    fileName = cms.string('L1Ntuple_fall15V2_2_wrongGT_db.root')
+)
+
+# Path and EndPath definitions
+process.load('JetMETCorrections.Configuration.JetCorrectors_cff')
+
+process.p = cms.Path(process.ak4PFCHSL1FastL2L3CorrectorChain + process.jetAnalysis)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.p,process.endjob_step)
--- JetAnalyser/python/run_DB_Service.py
+++ JetAnalyser/python/run_DB_Service.py
@@ -1,3 +1,13 @@
+"""
+This config is for running with JEC in a SQL file.
+I've also added in the JetCorrector Services incase that's needed.
+
+Note that we purposely use the *wrong* GlobalTag to ensure it's actually using the SQL file!
+
+GT:
+74X_mcRun2_asymptotic_v5 :  Summer15_25nsV7
+76X_mcRun2_asymptotic_RunIIFall15DR76_v1 : Fall15_25nsV2
+"""
 # Auto generated configuration file
 # using: 
 # Revision: 1.19 
@@ -7,7 +17,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
 
-process = cms.Process('RAW2DIGI',eras.Run2_2016)
+process = cms.Process('JET',eras.Run2_2016)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -21,14 +31,6 @@ process.load('Configuration.StandardSequences.RawToDigi_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 
-process.MessageLogger.cerr.threshold = 'INFO'
-process.MessageLogger.categories.append('JetAnal')
-process.MessageLogger.cerr.INFO = cms.untracked.PSet(
-            limit = cms.untracked.int32(1)
-            )
-
-
-
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(2)
 )
@@ -36,7 +38,7 @@ process.maxEvents = cms.untracked.PSet(
 # Input source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('/store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/AODSIM/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/1E2B5203-7AAA-E511-B6FD-0CC47A4D762A.root'),
-    secondaryFileNames = cms.untracked.vstring('/store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/08F09D12-11AA-E511-9613-0025905A60D0.root')
+    # secondaryFileNames = cms.untracked.vstring('/store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/08F09D12-11AA-E511-9613-0025905A60D0.root')
 )
 
 process.options = cms.untracked.PSet(
@@ -63,24 +65,26 @@ process.GlobalTag = GlobalTag(process.GlobalTag, '74X_mcRun2_asymptotic_v5', '')
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 from CondCore.DBCommon.CondDBSetup_cfi import *
 process.jec = cms.ESSource("PoolDBESSource",
-      DBParameters = cms.PSet(
+    DBParameters = cms.PSet(
         messageLevel = cms.untracked.int32(0)
         ),
-      timetype = cms.string('runnumber'),
-      toGet = cms.VPSet(
-      cms.PSet(
+    timetype = cms.string('runnumber'),
+    toGet = cms.VPSet(
+        cms.PSet(
             record = cms.string('JetCorrectionsRecord'),
-#            tag    = cms.string('JetCorrectorParametersCollection_Summer15_25nsV7_MC_AK4PFchs'),
+            # tag    = cms.string('JetCorrectorParametersCollection_Summer15_25nsV7_MC_AK4PFchs'),
             tag    = cms.string('JetCorrectorParametersCollection_Fall15_25nsV2_MC_AK4PFchs'),
             label  = cms.untracked.string('AK4PFCHS')
             ),
-      ), 
-#      connect = cms.string('sqlite:Summer15_25nsV7_MC.db')
-      connect = cms.string('sqlite:Fall15_25nsV2_MC.db')
+        ),
+    # connect = cms.string('sqlite:Summer15_25nsV7_MC.db')
+    connect = cms.string('sqlite:Fall15_25nsV2_MC.db')
 )
+
 ## add an es_prefer statement to resolve a possible conflict from simultaneous connection to a global tag
 process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
 
+# Add in the ESProducers - dunno if necessary?
 process.ak4PFCHSL1Fastjet = cms.ESProducer(
     'L1FastjetCorrectionESProducer',
     level       = cms.string('L1FastJet'),
@@ -108,24 +112,21 @@ process.ak4PFCHSL2L3 = cms.ESProducer(
 
 
 process.ak4PFCHSL1FastL2L3 = process.ak4PFCHSL2L3.clone()
-process.ak4PFCHSL1FastL2L3.correctors.insert(0,'ak4PFCHSL1Fastjet')
+process.ak4PFCHSL1FastL2L3.correctors.insert(0, 'ak4PFCHSL1Fastjet')
 
-process.jetAnal = cms.EDAnalyzer('JetAnalyser')
+process.jetAnalysis = cms.EDAnalyzer('JetAnalyser')
 
 process.TFileService = cms.Service(
     "TFileService",
-    #fileName = cms.string('L1Ntuple.root')
-    #fileName = cms.string('L1Ntuple_summer15V7_200_wrongGT_db.root')
-    fileName = cms.string('L1Ntuple_fall15V2_2_wrongGT_db.root')
+    # fileName = cms.string('L1Ntuple_summer15V7_200_wrongGT_db_service.root')
+    fileName = cms.string('L1Ntuple_fall15V2_2_wrongGT_db_service.root')
 )
 
 # Path and EndPath definitions
 process.load('JetMETCorrections.Configuration.JetCorrectors_cff')
 
-process.raw2digi_step = cms.Path(process.ak4PFCHSL1FastL2L3CorrectorChain+process.jetAnal)
+process.p = cms.Path(process.ak4PFCHSL1FastL2L3CorrectorChain + process.jetAnalysis)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.raw2digi_step,process.endjob_step)
-
-
+process.schedule = cms.Schedule(process.p,process.endjob_step)
